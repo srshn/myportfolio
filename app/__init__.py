17f7daddfaa5f023b4a626b86f270ecf1e0c34f1
Sasha Roshan <sasharshn@gmail.com>
2022-06-02 16:48:48 -0400

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/home')
+def home():
+    return render_template('home.html', title="Home Page", url=os.getenv("URL"))
