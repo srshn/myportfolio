f30a74c757d2b4522c4f5d48fe8fbd02165bcd23
lauren-ciha <64796985+Lauren-Ciha@users.noreply.github.com>
2022-06-03 12:44:57 -0500

    Basic template pattern for personal pages

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+name = "Name"
+experience_list = ["Experience 1", "Experience 2", "Experience 3"]
+education_list = ["Education 1"]
+hobby_list = ["Hobby 1", "Hobby 2", "Hobby 3", "Hobby 4"]
+
+# Example for one person
+# name = "Lauren Ciha"
+# experience_list = ["Schloss Visual Reasoning Lab", "People and Robots Lab", "MLH Fellowship!"]
+# education_list = ["University of Wisconsin-Madison"]
+# hobby_list = ["reading", "journaling", "running"]
 
 @app.route('/')
 def index():
@@ -13,3 +23,11 @@ def index():
 @app.route('/home')
 def home():
     return render_template('home.html', title="Home Page", url=os.getenv("URL"))
+
+@app.route('/sample')
+def template_test():
+    return render_template('sample_page.html', name = name, experience_list=experience_list, education_list=education_list, hobby_list = hobby_list, url=os.getenv("URL"))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
