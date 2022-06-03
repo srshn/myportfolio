da3aa4792ddd091d09d5517e2e7fa6da467fab36
Sasha Roshan <sasharshn@gmail.com>
2022-06-03 16:22:32 -0400

    This commit has code adding three additional pages with buttons, and a stationary menu in the index page linking to the additional pages

--- app/__init__.py
+++ app/__init__.py
@@ -8,35 +8,54 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="Index Page", url=os.getenv("URL"))
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     if request.method=='POST':
         if request.form.get('homeButton') == "Home" :
             return render_template('index.html', title="Home Page", url=os.getenv("URL"))
-        elif request.method=='GET' :
-            form=form
-            return render_template('sasha.html', form=form)
+        elif request.form.get('sashaPage') == "sasha" :
+            return render_template('sasha.html', title="Sasha Page", url=os.getenv("URL"))
+        elif request.form.get('laurenPage') == "lauren" :
+            return render_template('lauren.html', title="Lauren Page", url=os.getenv("URL"))
+        if request.form.get('ruchikaPage') == "ruchika" :
+            return render_template('ruchika.html', title="Ruchika Page", url=os.getenv("URL"))
+    elif request.method=='GET' :
+        form=form
+        return render_template('index.html', form=form)
         
-    return render_template('sasha.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="The Home Page", url=os.getenv("URL"))
 
 @app.route('/sasha', methods=['GET', 'POST'])
 def sasha():
     if request.method == 'POST':
         if request.form.get('homeButton') == "Home" :
             return render_template('index.html', title="Home Page", url=os.getenv("URL"))
-        elif request.method=='GET':
-            form=form
-            return render_template('sasha.html', title="MLH Fellow", url=os.getenv("URL"))
+    elif request.method=='GET':
+        form=form
+        return render_template('sasha.html', title="Sasha Page", url=os.getenv("URL"))
    
-    return render_template('sasha.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('sasha.html', title="Sasha Page", url=os.getenv("URL"))
 
 @app.route('/lauren', methods= ['POST', 'GET'])
 def lauren():
     if request.method == 'POST':
+       if request.form.get('homeButton') == "Home" :
+            return render_template('index.html', title="Home Page", url=os.getenv("URL"))
+    elif request.method=='GET':
+        form=form
+        return render_template('lauren.html', title="Lauren Page", url=os.getenv("URL"))
+    
+    return render_template('lauren.html', title="Lauren Page", url=os.getenv("URL"))
+
+@app.route('/ruchika', methods= ['POST', 'GET'])
+def ruchika():
+    if request.method == 'POST':
         if request.form.get('homeButton') == "Home" :
             return render_template('index.html', title="Home Page", url=os.getenv("URL"))
-        elif request.method=='GET':
-            form=form
-            return render_template('lauren.html', title="MLH Fellow", url=os.getenv("URL"))
+    elif request.method=='GET':
+        form=form
+        return render_template('ruchika.html', title="Ruchika Page", url=os.getenv("URL"))
+    
+    return render_template('ruchika.html', title="Ruchika Page", url=os.getenv("URL"))
