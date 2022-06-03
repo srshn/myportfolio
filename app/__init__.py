4b0bd38c278f7d0c1335cb190c8fab2b23be48b2
srshn <sasharshn@gmail.com>
2022-06-03 01:52:27 -0400

    adding pages and buttons for them, will update init.py shortly
    adding pages and buttons for them, will update init.py shortly

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,33 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html', title="Home Page", url=os.getenv("URL"))
+    if request.method=='POST':
+        if request.form.get('homeButton') == "Home" :
+            return render_template('index.html', title="Home Page", url=os.getenv("URL"))
+        elif request.method=='GET' :
+            form=form
+            return render_template('sasha.html', form=form)
+        
+    return render_template('sasha.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/sasha', methods=['GET', 'POST'])
+def sasha():
+    if request.method == 'POST':
+        if request.form.get('homeButton') == "Home" :
+            return render_template('index.html', title="Home Page", url=os.getenv("URL"))
+        elif request.method=='GET':
+            form=form
+            return render_template('sasha.html', title="MLH Fellow", url=os.getenv("URL"))
+   
+    return render_template('sasha.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/lauren', methods= ['POST', 'GET'])
+def lauren():
+    if request.method == 'POST':
+        if request.form.get('homeButton') == "Home" :
+            return render_template('index.html', title="Home Page", url=os.getenv("URL"))
+        elif request.method=='GET':
+            form=form
+            return render_template('lauren.html', title="MLH Fellow", url=os.getenv("URL"))
